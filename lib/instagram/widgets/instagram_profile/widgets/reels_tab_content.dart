cbfb550250f5c090600eaaec25233fa89faa70d0
ikrupp <akiberq@gmail.com>
2021-09-05 01:27:39 +0400

    Add base implementation of reels tab content

--- lib/instagram/widgets/instagram_profile/widgets/reels_tab_content.dart
+++ lib/instagram/widgets/instagram_profile/widgets/reels_tab_content.dart
@@ -5,6 +5,18 @@ class ReelsTabContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 3,
+        childAspectRatio: 0.5,
+        crossAxisSpacing: 1,
+        mainAxisSpacing: 1,
+      ),
+      itemBuilder: (context, index) {
+        return Container(
+          color: Colors.accents[(index + 5) % Colors.accents.length],
+        );
+      },
+    );
   }
 }
