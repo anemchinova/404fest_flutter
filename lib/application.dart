d50e6f6876a6753ae4fb0d6cc4b06f469d0f1042
ikrupp <akiberq@gmail.com>
2021-09-25 19:39:57 +0400

    Fix application brightness

--- lib/application.dart
+++ lib/application.dart
@@ -1,6 +1,7 @@
 import 'package:fest404/instagram/data/instagram_profile_data.dart';
 import 'package:fest404/onboard/onboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class Application extends StatelessWidget {
@@ -10,20 +11,23 @@ class Application extends StatelessWidget {
   Widget build(BuildContext context) {
     return Provider.value(
       value: mercuryProfile,
-      child: MaterialApp(
-        title: '404 Demo App',
-        theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.blue,
+      child: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: MaterialApp(
+          title: '404 Demo App',
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            primarySwatch: Colors.blue,
+          ),
+          builder: (context, child) {
+            return ScrollConfiguration(
+              behavior: const ScrollBehavior().copyWith(overscroll: false),
+              child: child ?? const SizedBox(),
+            );
+          },
+          home: const OnboardScreen(),
+          debugShowCheckedModeBanner: false,
         ),
-        builder: (context, child) {
-          return ScrollConfiguration(
-            behavior: const ScrollBehavior().copyWith(overscroll: false),
-            child: child ?? const SizedBox(),
-          );
-        },
-        home: const OnboardScreen(),
-        debugShowCheckedModeBanner: false,
       ),
     );
   }
